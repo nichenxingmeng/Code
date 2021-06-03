85a90f5347b431c61e5cb2dedfaa06ebfdf8aea2
nichenxingmeng <61072279+nichenxingmeng@users.noreply.github.com>
2021-06-03 23:36:50 +0800

    Add files via upload

--- 王道机试/二叉树.py
+++ 王道机试/二叉树.py
@@ -1,8 +1,47 @@
-def CountNodes(m, n):
-    if m > n:
-        return 0
-    else:
-        return 1 + CountNodes(m * 2, n) + CountNodes(m * 2 + 1, n)
+class TreeNode:
+    def __init__(self, data, leftChild = None, rightChild = None):
+        self.data = data
+        self.leftChild = leftChild
+        self.rightChild = rightChild
 
-m, n = map(int, input().strip().split())
-print(CountNodes(m, n))
+root = TreeNode(data)
+
+def PreOrder(root):
+    if root == None:
+        return
+
+    Visit(root.data)
+    PreOrder(root.leftChild)
+    PreOrder(root.rightChild)
+
+def InOrder(root):
+    if root == None:
+        return
+
+    InOrder(root.leftChild)
+    Visit(root.data)
+    InOrder(root.rightChild)
+
+def PostOrder(root):
+    if root == None:
+        return
+
+    PostOrder(root.leftChild)
+    PostOrder(root.rightChild)
+    Visit(root.data)
+
+def Visit(data):
+    print(data)
+
+def LevelOrder(root):
+    Q = []
+    if root:
+        Q.append(root)
+
+    while Q:
+        current = Q.pop(0)
+        Visit()
+        if current.leftChild:
+            Q.append(current.leftChild)
+        if current.rightChild:
+            Q.append(current.rightChild)
